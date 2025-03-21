e26be6144d0e3918f2de4c607649b428132efdfe
Kugesz <kovacsgergo.algyo@gmail.com>
2025-03-21 21:29:48 +0100

    Implement main script to process data files and save output to a text file

---
+++ main.py
@@ -0,0 +1,23 @@
+from functions.get_files_path import get_files_path
+from functions.get_data_from_files import get_data_from_files
+from functions.convert_data import convert_data
+# import save_data
+
+import config
+
+initDB_path, model_paths = get_files_path(config.FOLDER_PATH)
+
+db_connections, models = get_data_from_files(initDB_path, model_paths)
+
+print(len(models))
+
+string = convert_data(db_connections, models)
+
+print(len(string))
+
+output_file_path = "output.txt"  # Specify the output file name
+
+with open(output_file_path, "w", encoding="utf-8") as file:
+    for table in string:
+      file.write(table)
+      file.write("\n")
