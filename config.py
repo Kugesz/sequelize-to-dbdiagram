7b6d105cb87313bc5b07702cb07ad44f5ee0f9b1
Kugesz <kovacsgergo.algyo@gmail.com>
2025-03-23 23:54:04 +0100

    Update FOLDER_PATH to new project directory

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-FOLDER_PATH = "/home/kugesz/Documents/sequelize-to-dbdiagram/test"
-
+#FOLDER_PATH = "/home/kugesz/Documents/sequelize-to-dbdiagram/test"
+FOLDER_PATH = r"C:\Users\kovac\Desktop\gdb\GBDLogistics-backend-main\src\database"
 FILE_TYPE = "ts" # .js / .ts
